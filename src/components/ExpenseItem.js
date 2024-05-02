2913e18d5665f36b2bddb6ae3a40bcd51773bfc0
yb <yb725v@naver.com>
2024-05-02 12:26:11 +0900

    3. 컴포넌트 분할하기

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,38 +1,25 @@
-import React from 'react'
-import './ExpenseItem.css';
+import React from "react";
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./UI/Card";
 
 // const ExpenseItem = (props) => {
 //디스트럭처링을 통해 props가 아닌 {title}로 바로 가져올 수 있다
-const ExpenseItem = ({title, price, date}) => {
-   
-   // 한자리 수를 두자리로 변환해주는 함수
-   const make2digit = (text) => {
-      return text.toString().padStart(2, '0');
-   }
+const ExpenseItem = ({ title, price, date }) => {
+  // 숫자를 화폐 표기법으로 변환
+  const formattedPrice = new Intl.NumberFormat("ko-KR").format(price);
 
-   // 날짜 format 변환 함수 정의
-   const makeFormattedDate = () => {
-      const year = date.getFullYear();
-      const month = date.getMonth();
-      const day = date.getDate();
-
-      return `${year}년 ${make2digit(month)}월 ${make2digit(day)}일`;
-   }
-
-   // 숫자를 화폐 표기법으로 변환
-   const formattedPrice = new Intl.NumberFormat('ko-KR').format(price);
-
-   return (
-      <div>
-         <div className='expense-item'>
-            <div>{makeFormattedDate()}</div>
-            <div className='expense-item__description'>
-               <h2>{title}</h2>
-               <div className='expense-item__price'>{formattedPrice}원</div>
-            </div>
-         </div>
+  return (
+    <Card className={"circle"}>
+      <div className="expense-item">
+        <ExpenseDate date={date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">{formattedPrice}원</div>
+        </div>
       </div>
-   )
-}
+    </Card>
+  );
+};
 
-export default ExpenseItem
+export default ExpenseItem;
