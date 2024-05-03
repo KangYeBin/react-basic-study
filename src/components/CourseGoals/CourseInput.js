a98db973c183b08ffad83120f19c4a8d91948aa3
yb <yb725v@naver.com>
2024-05-03 15:07:05 +0900

    14. styled componenet 사용

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -29,7 +29,7 @@ const CourseInput = ({ onAdd }) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
         <label>나의 목표</label>
         <input
           style={{
