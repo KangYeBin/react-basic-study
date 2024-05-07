10f89353c82c38c477bee8b480cc6de4429958f9
yb <yb725v@naver.com>
2024-05-07 16:40:54 +0900

    24. useContext와 리팩토링

--- src/components/Store/auth-context.js
+++ src/components/Store/auth-context.js
@@ -3,6 +3,7 @@ import React from 'react';
 // 로그인 상태 변수를 관리할 컨텍스트 (중앙 저장소)
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {}, // 함수의 경우에도 Context에 저장 가능
 });
 
 export default AuthContext;
